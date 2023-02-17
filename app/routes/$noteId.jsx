8596828088d2474e5d9dc03190ed6b218c7b7913
Gurpreet Singh <gurpreet.singh19.gs@gmail.com>
2023-02-17 16:35:18 +0000

    feat/ added dynamic routign for the notes section. Now when clicking on the notes cards, the noteId corresponding params is added to the url.

--- app/routes/$noteId.jsx
+++ app/routes/$noteId.jsx
@@ -1,15 +1,29 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import styles from '../styles/note-details.css'
+import { getStoredNotes } from "../data/notes";
 
 export default function NoteDetailsPage() {
+    const note = useLoaderData()
     return (
         <main id='note-details'>
             <header>
                 <nav>
                 <Link to='/notes'>Back to all the notes</Link>
             </nav>
-            <h1>Dynamic Note title here</h1>
+            <h1>{note.title}</h1>
             </header>
-            <p id='note-details-content'>Note content dynamic</p>
+            <p id='note-details-content'>{note.content}</p>
         </main>
     )
+}
+
+export async function loader({params}) {
+  const notes = await getStoredNotes();
+  const noteId = params.noteId
+  const selectedNotes = notes.find(note => note.id === noteId )
+  return selectedNotes;
+}
+
+export function links(){
+    return [{rel: 'stylesheet', href: styles}]
 }
